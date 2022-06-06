bfdffecda4afb5ed44b016f078113f863bb6ec01
sun <jannis.dolleck@gmail.com>
2022-06-06 18:57:47 +0200

    "Just Testing"

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World! Still there?')
-})
+app.get('/demo', (req, res) => {
+  res.set("x-full-stack", "4life")
+  res.status(418);
+  res.send("I prefer coffee")
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
